7147b796f414b00d6a924f9afe12b6540bc6623d
mathildeew <mathildewiik@hotmail.com>
2023-02-03 21:44:09 +0100

    adds auction/bids carousel

--- src/js/script.js
+++ src/js/script.js
@@ -4,3 +4,35 @@ const myInput = document.getElementById("myInput");
 myModal.addEventListener("shown.bs.modal", () => {
   myInput.focus();
 });
+
+// const auctionsBtn = document.getElementById("showAuctions");
+// const auctionsCards = document.getElementById("auctionsCards");
+// const bidsBtn = document.getElementById("showBids");
+// const bidsCards = document.getElementById("bidsCards");
+
+// auctionsBtn.style.textDecoration = "underline";
+// auctionsCards.style.display = "flex";
+// bidsCards.style.display = "none";
+
+// auctionsBtn.addEventListener("click", () => {
+//   showAuctions();
+// });
+
+// function showAuctions() {
+//   auctionsBtn.style.textDecoration = "underline";
+//   bidsBtn.style.textDecoration = "none";
+//   auctionsCards.style.display = "flex";
+//   bidsCards.style.display = "none";
+// }
+
+// bidsBtn.addEventListener("click", () => {
+//   showBids();
+// });
+
+// function showBids() {
+//   bidsCards.style.display = "flex";
+
+//   bidsBtn.style.textDecoration = "underline";
+//   auctionsBtn.style.textDecoration = "none";
+//   auctionsCards.style.display = "none";
+// }
