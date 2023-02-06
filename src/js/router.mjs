cc8e72757e8c57cde9e51745a949404238ec616b
mathildeew <mathildewiik@hotmail.com>
2023-02-06 17:18:21 +0100

    adds router for all pages

---
+++ src/js/router.mjs
@@ -0,0 +1,22 @@
+// Home
+import { newAuctionModal } from "./home/newAuctionModal.mjs";
+
+// Run function based on pathname
+const path = location.pathname;
+
+switch (path) {
+  case "/":
+    break;
+
+  case "/profile/":
+    break;
+
+  case "/profile/auction/":
+    break;
+
+  case "/accounts/login/":
+    break;
+
+  case "/accounts/emailsignup/":
+    break;
+}
