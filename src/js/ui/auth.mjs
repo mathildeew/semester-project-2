86bdb8931854d4d50584cc542434545da4cde0ac
mathildeew <mathildewiik@hotmail.com>
2023-02-08 20:44:21 +0100

    new auction button hides when not logged in

--- src/js/ui/auth.mjs
+++ src/js/ui/auth.mjs
@@ -1,8 +1,5 @@
-import * as storage from "../storage/localStorage.mjs";
 import { unauthorizedBtn } from "./unauth/auctionBtn.mjs";
 import { unauthorizedNav } from "./unauth/nav.mjs";
-
-const token = storage.get("token");
 
 export function auth() {
   unauthorizedNav();
--- src/js/ui/unauth/auctionBtn.mjs
+++ src/js/ui/unauth/auctionBtn.mjs
@@ -1,4 +1,9 @@
-const newAuctionBtn = document.querySelector(".newAuctionBtn");
+import * as storage from "../../storage/localStorage.mjs";
+
 export function unauthorizedBtn() {
-  newAuctionBtn.style.display = "none";
+  const newAuctionBtn = document.querySelector(".newAuctionBtn");
+  const token = storage.get("token");
+  if (token === undefined || token === null || token === "") {
+    newAuctionBtn.style.display = "none";
+  }
 }
