2c7a768d04fe0654fe0ecfe3d1218895180926dc
mathildeew <mathildewiik@hotmail.com>
2023-02-08 20:50:57 +0100

    search function works

--- src/js/home/getAuctions.mjs
+++ src/js/home/getAuctions.mjs
@@ -1,10 +1,13 @@
 import { baseUrl } from "../api/apiUrls.mjs";
 import { fetchOptions } from "../api/fetchOptions.mjs";
 import { displayAuctions } from "./display.mjs";
+import { search } from "./search.mjs";
 
 export async function getAuctions() {
   const [getData, postData] = fetchOptions;
   const response = await fetch(`${baseUrl}/auction/listings`, getData);
   const json = await response.json();
   displayAuctions(json);
+  search(json);
+  console.log(json);
 }
