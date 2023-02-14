34d355c4fba45139b8fe3d2358be7c09cd9dcd54
mathildeew <mathildewiik@hotmail.com>
2023-02-14 19:58:15 +0100

    no bids text if theres no bid to auction

--- src/js/auction/display.mjs
+++ src/js/auction/display.mjs
@@ -36,7 +36,7 @@ export function displayAuction(auction) {
                           <img />
                           <a id="sellerButton" class="bg-grey d-flex align-items-center rounded-pill mt-1 ms-1">
                             <img class="rounded-circle border border-dark"/>
-                            <p class="mb-0 ms-1"></p>
+                            <p class="mb-0 ms-2 me-3"></p>
                           </a>
                         <img class="rounded mb-2" />
                         <p class="fs-4 mb-0"></p>
@@ -58,23 +58,24 @@ export function displayAuction(auction) {
 
   function showBids(auction) {
     const bids = auction.bids;
-
-    for (let i = 0; i < bids.length; i++) {
-      const sortedBids = bids.sort((a, b) => {
-        return b.amount - a.amount;
-      });
-
-      const created = new Date(sortedBids[i].created).toLocaleString();
-
-      const bidsContainer = document.getElementById("bidsHistory");
-      if (bids.length <= 0) {
-        // bidsContainer.style.display = "none";
-      }
-
-      const bidsHistory = document.createElement("div");
-      bidsHistory.className =
-        "bg-grey d-flex align-items-center justify-content-between px-4 mb-2 rounded-pill";
+    const bidsContainer = document.getElementById("bidsHistory");
+    if (bids.length <= 0) {
       bidsHistory.innerHTML += `
+      <p class="bg-grey px-3 mb-3 rounded-pill">No bids</p>
+      `;
+    } else {
+      for (let i = 0; i < bids.length; i++) {
+        const sortedBids = bids.sort((a, b) => {
+          return b.amount - a.amount;
+        });
+
+        const created = new Date(sortedBids[i].created).toLocaleString();
+
+        const bidsHistory = document.createElement("div");
+        bidsHistory.className =
+          "bg-grey d-flex align-items-center justify-content-between px-4 mb-3 rounded-pill";
+
+        bidsHistory.innerHTML += `
                                   <div>
                                     <p class="fw-bold mb-0"></p>
                                     <p class="mb-0"></p>
@@ -84,14 +85,15 @@ export function displayAuction(auction) {
                                   </div>
                                   `;
 
-      bidsHistory.querySelector("div p:nth-child(1)").innerText =
-        sortedBids[i].bidderName;
-      bidsHistory.querySelector("div p:nth-child(2)").innerText = created;
-      bidsHistory.querySelector(
-        "div:nth-child(2) p"
-      ).innerText = `$${sortedBids[i].amount}`;
+        bidsHistory.querySelector("div p:nth-child(1)").innerText =
+          sortedBids[i].bidderName;
+        bidsHistory.querySelector("div p:nth-child(2)").innerText = created;
+        bidsHistory.querySelector(
+          "div:nth-child(2) p"
+        ).innerText = `$${sortedBids[i].amount}`;
 
-      bidsContainer.append(bidsHistory);
+        bidsContainer.append(bidsHistory);
+      }
     }
   }
   showBids(auction);
