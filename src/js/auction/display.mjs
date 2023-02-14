3daf9edd9934f2a3ef875bd3ebc5ca7245a5de4d
mathildeew <mathildewiik@hotmail.com>
2023-02-14 20:54:30 +0100

    shows highest bid and hides bids history if theres no bids

--- src/js/auction/display.mjs
+++ src/js/auction/display.mjs
@@ -28,23 +28,25 @@ export function displayAuction(auction) {
   } else {
   }
 
-  const auctionCard = document.getElementById("auctionCard");
+  const auctionCard = document.getElementById("auctionCards");
   const cardBody = document.createElement("div");
   cardBody.className = "d-flex flex-column";
   cardBody.innerHTML = `
                         <div class="d-flex flex-column">
-                          <img />
+                          <img class="rounded"/>
                           <a id="sellerButton" class="bg-grey d-flex align-items-center rounded-pill mt-1 ms-1">
                             <img class="rounded-circle border border-dark"/>
                             <p class="mb-0 ms-2 me-3"></p>
                           </a>
                         <img class="rounded mb-2" />
-                        <p class="fs-4 mb-0"></p>
+                        <p class="fs-3 fw-bold mb-2"></p>
                         <div class="d-flex">
                           <i class="bi bi-clock-fill me-1"></i>
                           <p><p>
                         </div>
-                        <p class=""></p>
+                        <p class="bg-primary w-75 fs-5 p-2 mb-3 rounded">No bids</p>
+                        <p class="fs-5 mb-0">Description</p>
+                        <p></p>
   `;
   cardBody.querySelector("a img").src = auction.seller.avatar;
   cardBody.querySelector("a p").innerText = auction.seller.name;
@@ -52,17 +54,21 @@ export function displayAuction(auction) {
   cardBody.querySelector("img").src = auction.media;
   cardBody.querySelector("p:nth-child(4)").innerText = auction.title;
   cardBody.querySelector("div:nth-child(5) p").innerText = timer;
-  cardBody.querySelector("p:nth-child(6)").innerText = auction.description;
+  if (auction.bids.length > 0) {
+    cardBody.querySelector("p:nth-child(6)").innerText = `Highest bid: $${
+      auction.bids[auction.bids.length - 1].amount
+    }`;
+  }
+  cardBody.querySelector("p:nth-child(8)").innerText = auction.description;
 
   auctionCard.append(cardBody);
 
   function showBids(auction) {
     const bids = auction.bids;
     const bidsContainer = document.getElementById("bidsHistory");
+    const bidsSection = document.getElementById("bids");
     if (bids.length <= 0) {
-      bidsHistory.innerHTML += `
-      <p class="bg-grey px-3 mb-3 rounded-pill">No bids</p>
-      `;
+      bidsSection.style.display = "none";
     } else {
       for (let i = 0; i < bids.length; i++) {
         const sortedBids = bids.sort((a, b) => {
