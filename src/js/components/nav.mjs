f1e73eabad04a1b6be8532a54942b14dc663cacf
mathildeew <mathildewiik@hotmail.com>
2023-02-14 09:28:52 +0100

    adds background image for nav button

--- src/js/components/nav.mjs
+++ src/js/components/nav.mjs
@@ -1,8 +1,11 @@
 import * as storage from "../storage/localStorage.mjs";
 
 const name = storage.get("name");
+const avatar = storage.get("avatar");
+const avatarContainer = document.getElementById("navBtn");
 
 export function nav() {
   const navCollapse = document.getElementById("navbarCollapse");
   navCollapse.querySelector("a:nth-child(1)").href = `/profile/?name=${name}`;
+  navBtn.style.backgroundImage = `url(${avatar})`;
 }
