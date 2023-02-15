d61508b9cfc94083e948e16d23d3b8d1d9661ab2
mathildeew <mathildewiik@hotmail.com>
2023-02-15 13:59:46 +0100

    deleted not working  modal functions

--- src/js/home/newAuctionModal.mjs
+++
@@ -1,8 +0,0 @@
-const newAuctionModal = document.getElementById("newAuctionModal");
-const newAuctionModalBtn = document.getElementById("newAuctionModalBtn");
-
-export function openNewAuctionModal() {
-  newAuctionModalBtn.addEventListener("shown.bs.modal", () => {
-    newAuctionModalBtn.focus();
-  });
-}
