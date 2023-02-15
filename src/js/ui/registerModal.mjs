e9be74ae7a70db6051bcd66c682fa33bfa87adde
mathildeew <mathildewiik@hotmail.com>
2023-02-15 14:11:15 +0100

    deleted unused function

--- src/js/ui/registerModal.mjs
+++
@@ -1,6 +0,0 @@
-const registeredModal = document.getElementById("registeredModal");
-const registerBtn = document.getElementById("registerBtn");
-
-registeredModal.addEventListener("shown.bs.modal", () => {
-  registerBtn.focus();
-});
