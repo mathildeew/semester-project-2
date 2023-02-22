953ba3a570518a24a9d58190c2b9e20d542e8654
mathildeew <mathildewiik@hotmail.com>
2023-02-22 19:18:56 +0100

    adds jsdocs to update auction

--- src/js/api/auction/update.mjs
+++ src/js/api/auction/update.mjs
@@ -5,7 +5,11 @@ import { fetchOptions } from "../../api/fetchOptions.mjs";
 const queryString = document.location.search;
 const params = new URLSearchParams(queryString);
 const id = params.get("id");
-
+/**
+ * Updates auction with new content
+ * @param {*} putContent Data sent to API
+ * @returns Response
+ */
 export async function updateAuction(putContent) {
   const [getData, postData, putData] = fetchOptions;
   putData["body"] = JSON.stringify(putContent);
