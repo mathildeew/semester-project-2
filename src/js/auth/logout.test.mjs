004af4eb81cd22e02cd0d53c317dcc6d043bf1d7
mathildeew <mathildewiik@hotmail.com>
2023-02-22 22:44:50 +0100

    adds unit test for logout, but logout needs refactoring

--- src/js/auth/logout.test.mjs
+++ src/js/auth/logout.test.mjs
@@ -1,1 +1,17 @@
+import { LocalStorageMock } from "../storage/localStorageMock";
+import { logout } from "./logout.mjs";
+
 const TEST_TOKEN = "tokentokentoken";
+
+global.localStorage = new LocalStorageMock();
+
+localStorage.setItem("token", TEST_TOKEN);
+
+describe("Logout", () => {
+  expect(localStorage.getItem("token")).toEqual(TEST_TOKEN);
+
+  it("Clears the token from localStorage", () => {
+    logout();
+    expect(localStorage.getItem("token")).toBeUndefined();
+  });
+});
