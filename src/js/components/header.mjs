ed7ef302f3de05e992c982a038fee6b889d73c5e
mathildeew <mathildewiik@hotmail.com>
2023-02-24 13:58:00 +0100

    styling for header and login animation

--- src/js/components/header.mjs
+++ src/js/components/header.mjs
@@ -1,34 +1,36 @@
-const headerContent = [
-  {
-    image: "../../../assets/catHeader.png",
-    text: "cat?",
-  },
-  {
-    image: "../../../assets/headerBooks.png",
-    text: "books?",
-  },
-  {
-    image: "../../../assets/headerJacket.png",
-    text: "jacket?",
-  },
-  {
-    image: "../../../assets/headerPlant.png",
-    text: "plant?",
-  },
-  {
-    image: "../../../assets/headerShoe.png",
-    text: "shoes?",
-  },
-];
+export function getRandom() {
+  const headerContent = [
+    {
+      image: "../../../assets/catHeader.png",
+      text: "cat?",
+    },
+    {
+      image: "../../../assets/headerBooks.png",
+      text: "books?",
+    },
+    {
+      image: "../../../assets/headerJacket.png",
+      text: "jacket?",
+    },
+    {
+      image: "../../../assets/headerPlant.png",
+      text: "plant?",
+    },
+    {
+      image: "../../../assets/headerShoe.png",
+      text: "shoes?",
+    },
+  ];
+
+  const randomIndex = Math.floor(Math.random() * headerContent.length);
+  const randomContent = headerContent[randomIndex];
+  return randomContent;
+}
 
 export function headerRandom() {
   const header = document.querySelector("header");
   const headerText = document.getElementById("headerOutline");
-
-  const randomIndex = Math.floor(Math.random() * headerContent.length);
-  const randomContent = headerContent[randomIndex];
-
-  console.log(randomContent);
+  const randomContent = getRandom();
 
   header.style.backgroundImage = `url(${randomContent.image})`;
   headerText.innerHTML = randomContent.text;
