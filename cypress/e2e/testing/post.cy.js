b437c1d5e0ea82618f060071396957b8670365d3
mathildeew <mathildewiik@hotmail.com>
2023-02-26 17:11:38 +0100

    e2e test passes for create auction

--- cypress/e2e/testing/post.cy.js
+++ cypress/e2e/testing/post.cy.js
@@ -3,6 +3,13 @@ const TEST_USER = {
   password: "Gurkemeie69",
 };
 
+const TEST_POST = {
+  title: "Selling my boat",
+  media:
+    "https://upload.wikimedia.org/wikipedia/commons/1/1a/Inflatable_boat_with_mercury_4hp_engine.JPG",
+  description: "Brand new :)",
+};
+
 describe("POST /json", () => {
   beforeEach(() => {
     cy.clearLocalStorage();
@@ -17,7 +24,6 @@ describe("POST /json", () => {
     cy.get("input[name=email]").type(TEST_USER.email);
     cy.get("input[name=password").type(TEST_USER.password);
     cy.get("#loginBtn").contains("Log in").click();
-
     cy.request("POST", "/json").then((response) => {
       expect(response.status).to.eq(200);
     });
@@ -28,7 +34,22 @@ describe("POST /json", () => {
       });
   });
 
-  //   it("Can create an auction", () => {});
-  //   it("Can place a bid on an auction", () => {});
-  //   it("Can register a new user", () => {});
+  it("Can create an auction", () => {
+    cy.get("#loginBtnHeader").click();
+    cy.get("#loginForm button").contains("Log in").click();
+    cy.wait(1000);
+    cy.get("input[name=email]").type(TEST_USER.email);
+    cy.get("input[name=password").type(TEST_USER.password);
+    cy.get("#loginBtn").contains("Log in").click();
+
+    cy.get("#newAuctionModalBtn").contains("New auction").click();
+    cy.get("input[name=title]").type(TEST_POST.title);
+    cy.get(" #mediaOne").type(TEST_POST.media);
+    cy.get("textarea[type=text]").type(TEST_POST.description);
+    cy.get("button").contains("Create auction").click();
+
+    cy.request("POST", "/json").then((response) => {
+      expect(response.status).to.eq(200);
+    });
+  });
 });
