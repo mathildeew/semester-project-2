683cadb99feaab02711656b4281ec02dae432568
mathildeew <mathildewiik@hotmail.com>
2023-02-27 19:31:05 +0100

    adds load more button

--- src/js/path/home/home.mjs
+++ src/js/path/home/home.mjs
@@ -4,19 +4,32 @@ import { display } from "./display.mjs";
 import { filter } from "./filter.mjs";
 import { search } from "./search.mjs";
 import * as storage from "../../storage/localStorage.mjs";
+import { auction } from "../auction/auction.mjs";
 
 export async function home() {
+  // Load more
+  const loadMoreBtn = document.getElementById("loadAuctions");
+
+  let limit = 5;
+
   const auctions = await get(
-    `${baseUrl}/auction/listings?sort=created&sortOrder=desc&_seller=true&_bids=true`
+    `${baseUrl}/auction/listings?sort=created&sortOrder=desc&_seller=true&_bids=true&_active=true&limit=${limit}`
   );
 
-  setTimeout(() => {
-    display(auctions);
-  }, "1000");
+  display(auctions);
 
+  loadMoreBtn.addEventListener("click", async () => {
+    limit = limit + 5;
+    const auctions = await get(
+      `${baseUrl}/auction/listings?sort=created&sortOrder=desc&_seller=true&_bids=true&_active=true&limit=${limit}`
+    );
+    display(auctions);
+  });
+
+  // Run search
   search(auctions);
 
-  //   Run filter function if user are logged in
+  // Run filter function if user are logged in
   const token = storage.get("token");
   const filterContainer = document.getElementById("filter");
   token ? filter(auctions) : (filterContainer.style.display = "none");
