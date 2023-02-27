c2d7c85e72baebe45f7804856885dcb0a0117f20
mathildeew <mathildewiik@hotmail.com>
2023-02-28 00:22:34 +0100

    started clean on profile

--- src/js/path/profile/displayAuctions.mjs
+++ src/js/path/profile/displayAuctions.mjs
@@ -21,21 +21,26 @@ export function displayAuctions(auctions) {
     // Display auctions
     const auctionsContainer = document.getElementById("auctionsProfile");
     const auctionCard = document.createElement("div");
-    auctionCard.className =
-      " bg-light rounded col-12 col-sm-9 col-md-5 col-lg-5 mb-3";
+    auctionCard.id = "auctionCardProfile";
+
+    auctionCard.className = " ";
     auctionCard.innerHTML += `
-                                      <a class="d-flex my-auto justify-content-start py-2">
-                                        <img id="auctionsImg" class="rounded" />
-                                        <div class="ms-3">
-                                          <p class="auctionCardTitle fw-bold mb-0"></p>
-                                          <p class="auctionCardSeller fw-light mb-3"></p>
-                                          <div class="d-flex mb-3">
-                                            <i class="bi bi-clock-fill me-1"></i>
-                                            <p class="auctionCardEnds mb-0"></p>
-                                          </div>
-                                          <p class="auctionCardHighestBid fs-5 fw-bold mb-0"></p>
-                                        </div>  
-                                      </a>
+                              <a>
+                              <div class="card h-100 bg-light border border-secondary">
+                                <div class="overflow-hidden rounded">
+                                 <img id="auctionsImg" class="card-img-top rouded" />
+                                </div>
+                                <div class="card-body d-flex flex-column justify-content-between">
+                                <div>
+                                  <h3 class="auctionCardTitle card-title"></h3>
+                                  <div class="d-flex mb-3">
+                                    <i class="bi bi-clock-fill me-1"></i>
+                                    <p class="auctionCardEnds mb-0"></p>
+                                  </div>
+                                  </div>
+                                </div>
+                              </div>
+                            </a>
                                     `;
 
     auctionCard.querySelector("a").href = `/auction/?id=${id}`;
