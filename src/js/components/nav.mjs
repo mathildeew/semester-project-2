0912c34ce2a0ff1e85f7aeba0815b8c328932acf
mathildeew <mathildewiik@hotmail.com>
2023-03-01 17:40:36 +0100

    last clean profile

--- src/js/components/nav.mjs
+++ src/js/components/nav.mjs
@@ -1,12 +1,12 @@
 import * as storage from "../storage/localStorage.mjs";
 
-const name = storage.get("name");
-const avatar = storage.get("avatar");
-const token = storage.get("token");
-
 export function nav() {
-  const navBtn = document.getElementById("profileLink");
+  const name = storage.get("name");
+  const avatar = storage.get("avatar");
+  const token = storage.get("token");
+  const navBtn = document.getElementById("navBtn");
+  const profileLink = document.getElementById("profileLink");
 
-  navBtn.querySelector("button").style.backgroundImage = `url(${avatar})`;
-  navBtn.href = `/profile/?name=${name}`;
+  navBtn.style.backgroundImage = `url(${avatar})`;
+  profileLink.href = `/profile/?name=${name}`;
 }
