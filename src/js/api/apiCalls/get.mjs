3609a13776b7627c333f6cbd7426e53c01025d6c
mathildeew <mathildewiik@hotmail.com>
2023-03-02 16:22:50 +0100

    adds jsdocs to get

--- src/js/api/apiCalls/get.mjs
+++ src/js/api/apiCalls/get.mjs
@@ -1,5 +1,10 @@
 import { fetchOptions } from "../fetchOptions.mjs";
 
+/**
+ * Sends a get request to the server
+ * @param {url} url
+ * @returns The API json
+ */
 export async function get(url) {
   const [getData, postData] = fetchOptions;
   const response = await fetch(url, getData);
