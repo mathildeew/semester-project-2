8f388010fe8a6a4984aa45479e1f7999e4407319
mathildeew <mathildewiik@hotmail.com>
2023-03-02 20:34:37 +0100

    adds jsdocs to login

--- src/js/api/apiCalls/auth/login.mjs
+++ src/js/api/apiCalls/auth/login.mjs
@@ -1,7 +1,18 @@
 import { setStorage } from "../../../storage/setStorage.mjs";
-import { baseUrl } from "../../apiUrls.mjs";
 import { fetchOptions } from "../../fetchOptions.mjs";
 
+/**
+ * Sends a POST request to the server
+ * @param {url} url Log in url
+ * @param {object} postContent The body of the request
+ * @returns The response from the request
+ * @example
+ * ```
+ * // Sends a POST request with log in credentials.
+ * // Returns the json as response
+ * const response = await login(`${baseUrl}/auction/auth/login`, postContent)
+ *```
+ */
 export async function login(url, postContent) {
   const [getData, postData] = fetchOptions;
   postData["body"] = JSON.stringify(postContent);
