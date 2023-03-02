ad32ade4610482d3f11b8cad9fe8bcf4aa2876a8
mathildeew <mathildewiik@hotmail.com>
2023-03-02 11:01:38 +0100

    display credits in nav

--- src/js/components/nav.mjs
+++ src/js/components/nav.mjs
@@ -4,9 +4,13 @@ export function nav() {
   const name = storage.get("name");
   const avatar = storage.get("avatar");
   const token = storage.get("token");
+  const credits = storage.get("credits");
+
   const navBtn = document.getElementById("navBtn");
   const profileLink = document.getElementById("profileLink");
+  const navCredits = document.getElementById("navCredits");
 
   navBtn.style.backgroundImage = `url(${avatar})`;
   profileLink.href = `/profile/?name=${name}`;
+  navCredits.innerText = `$${credits}`;
 }
