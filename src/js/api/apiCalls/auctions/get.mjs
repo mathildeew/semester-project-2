7b62c48ca1a8e1bba900f03a8c1309ede48c781b
mathildeew <mathildewiik@hotmail.com>
2023-03-02 22:34:26 +0100

    refactored getall auctions

---
+++ src/js/api/apiCalls/auctions/get.mjs
@@ -0,0 +1,29 @@
+import { fetchOptions } from "../../fetchOptions.mjs";
+
+/**
+ * Sends a GET request to the server
+ * @param {url} url All auction Url
+ * @returns The API json
+ * @example
+ * ```
+ * // Sends a GET request to the server
+ * // Returns json if the response.ok
+ * // Shows error message if !response.ok
+ * const json = await getAll(
+    `${baseUrl}/auction/listings?sort=created&sortOrder=desc&_seller=true&_bids=true&_active=true`
+  );
+ * ```
+ */
+export async function getAll(url) {
+  const [getData, postData] = fetchOptions;
+  const response = await fetch(url, getData);
+  const json = await response.json();
+
+  const errorMessage = document.getElementById("errorMessageAPI");
+
+  if (response.ok) {
+    return json;
+  } else {
+    errorMessage.style.display = "flex";
+  }
+}
--- src/js/path/home/home.mjs
+++ src/js/path/home/home.mjs
@@ -1,48 +1,44 @@
 import { baseUrl } from "../../api/apiUrls.mjs";
-import { get } from "../../api/apiCalls/get.mjs";
 import { display } from "../../render/renderAuctions.mjs";
 import { filter } from "../../func/filter.mjs";
 import { search } from "../../func/search.mjs";
 import { nav } from "../../components/nav.mjs";
 import { header } from "../../render/header.mjs";
 import { unauth } from "../../auth/unauthHome.mjs";
+import { getAll } from "../../api/apiCalls/auctions/get.mjs";
 
 export async function home() {
   nav();
   header();
   unauth();
-  const loadMoreBtn = document.getElementById("loadAuctions");
   const loader = document.getElementById("loader");
   const errorMessage = document.getElementById("errorMessageAPI");
 
   loader.style.display = "none";
   errorMessage.style.display = "none";
 
-  try {
-    let limit = 24;
-    const auctions = await get(
-      `${baseUrl}/auctionPP/listings?sort=created&sortOrder=desc&_seller=true&_bids=true&_active=true&limit=${limit}`
-    );
-    // const allAuctions = await get(
-    //   `${baseUrl}/auction/listings?sort=created&sortOrder=desc&_seller=true&_bids=true&_active=true`
-    // );
-    // const endedAuctions = await get(
-    //   `${baseUrl}/auction/listings?sort=created&sortOrder=desc&_seller=true&_bids=true`
-    // );
+  let limit = 24;
+  const auctions = await getAll(
+    `${baseUrl}/auction/listings?sort=created&sortOrder=desc&_seller=true&_bids=true&_active=true&limit=${limit}`
+  );
+  display(auctions);
 
+  // const allAuctions = await get(
+  //   `${baseUrl}/auction/listings?sort=created&sortOrder=desc&_seller=true&_bids=true&_active=true`
+  // );
+  // const endedAuctions = await get(
+  //   `${baseUrl}/auction/listings?sort=created&sortOrder=desc&_seller=true&_bids=true`
+  // );
+
+  // search();
+  // filter(auctions, allAuctions, endedAuctions);
+  const loadMoreBtn = document.getElementById("loadAuctions");
+
+  loadMoreBtn.addEventListener("click", async () => {
+    limit = limit + 6;
+    const auctions = await getAll(
+      `${baseUrl}/auction/listings?sort=created&sortOrder=desc&_seller=true&_bids=true&_active=true&limit=${limit}`
+    );
     display(auctions);
-    search();
-    // filter(auctions, allAuctions, endedAuctions);
-    loadMoreBtn.addEventListener("click", async () => {
-      limit = limit + 6;
-      const auctions = await get(
-        `${baseUrl}/auction/listings?sort=created&sortOrder=desc&_seller=true&_bids=true&_active=true&limit=${limit}`
-      );
-      display(auctions);
-    });
-  } catch (error) {
-    errorMessage.style.display = "inline";
-    loadMoreBtn.style.display = "none";
-    console.log(error);
-  }
+  });
 }
