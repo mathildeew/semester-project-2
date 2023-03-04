255d72ef3a6f7a1bf8ed35a341d30a9c74f1f8d8
mathildeew <mathildewiik@hotmail.com>
2023-03-04 20:54:59 +0100

    adds get func for single auction and auctiontemplate to make loader cover screen

--- src/js/render/renderAuction.mjs
+++ src/js/render/renderAuction.mjs
@@ -1,10 +1,23 @@
 import { auctionCarousel } from "./carousel.mjs";
 import { initializeCountdown } from "../func/timers/countdown.mjs";
+import { auctionTemplate } from "../templates/auctionIDTemplate.mjs";
 
 export function renderAuction(auction) {
   document.title += ` ${auction.title}`;
   document.head.querySelector("meta[name=description]").content =
     auction.description;
+
+  const auctionDetails = {
+    avatar: auction.seller.avatar,
+    title: auction.title,
+    seller: auction.seller,
+    desc: auction.description,
+    created: auction.created,
+    updated: auction.updated,
+  };
+
+  const auctionContainer = document.getElementById("auction");
+  auctionContainer.innerHTML = auctionTemplate();
 
   // Display auction
   const auctionTitle = document.getElementById("singleAuctionTitle");
