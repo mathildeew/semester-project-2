8c7a6f236230c76cc2571c7c903f86aa84fb49c2
mathildeew <mathildewiik@hotmail.com>
2023-03-05 15:27:44 +0100

    updates jsdocs and remove unused constants and functions

--- src/js/api/apiCalls/auctions/update.mjs
+++ src/js/api/apiCalls/auctions/update.mjs
@@ -1,27 +1,24 @@
-import * as storage from "../../../storage/localStorage.mjs";
 import { fetchOptions } from "../../fetchOptions.mjs";
 
 /**
  * Sends a PUT request to the API server
- * @param {url} url The API url
+ * @param {url} url The API url t
  * @param {object} putContent The request body
  * @returns The response from the request
  * @example
  * ```
- * // Sends a PUT request with the updated posts content
+ * // Sends a PUT request with the updated auctions content
  * // Auction is reloaded if successfull
- *    const response = await updateAuction(
-      `${baseUrl}/auction/listings/${id}`, putContent);
+ *    const response = await updateAuction(`${baseUrl}/auction/listings/${id}`, putContent);
  * ````
  */
 export async function updateAuction(url, putContent) {
   const [getData, postData, putData] = fetchOptions;
   putData["body"] = JSON.stringify(putContent);
+
   const response = await fetch(url, putData);
-  const json = await response.json();
 
   if (response.ok) {
     window.location.reload();
   }
-  return json;
 }
