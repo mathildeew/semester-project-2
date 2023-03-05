c3a47a54ecc617e494a22aaa812b2b310dcb053c
mathildeew <mathildewiik@hotmail.com>
2023-03-05 16:11:33 +0100

    moved errormessages for updateauclistnr to apicall

--- src/js/api/apiCalls/auctions/update.mjs
+++ src/js/api/apiCalls/auctions/update.mjs
@@ -9,6 +9,7 @@ import { fetchOptions } from "../../fetchOptions.mjs";
  * ```
  * // Sends a PUT request with the updated auctions content
  * // Auction is reloaded if successfull
+ * // Error messages is showed if not succesfull
  *    const response = await updateAuction(`${baseUrl}/auction/listings/${id}`, putContent);
  * ````
  */
@@ -17,8 +18,20 @@ export async function updateAuction(url, putContent) {
   putData["body"] = JSON.stringify(putContent);
 
   const response = await fetch(url, putData);
+  const json = await response.json();
+
+  console.log(json);
 
   if (response.ok) {
     window.location.reload();
+  }
+
+  if (json.errors) {
+    const updateBtn = document.getElementById("updateAuctionBtn");
+    const errorMessage = document.createElement("p");
+    errorMessage.className = "text-danger col-10 mt-3";
+    errorMessage.innerHTML = `${json.errors[0].message}`;
+    updateAuctionForm.append(errorMessage);
+    updateBtn.innerText = "Update";
   }
 }
