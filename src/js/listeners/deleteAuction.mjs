eb2389b21f129f6aa245a154f25379f5e24339f0
mathildeew <mathildewiik@hotmail.com>
2023-03-05 16:02:58 +0100

    moves errormessage from deletelistener to apicall

--- src/js/listeners/deleteAuction.mjs
+++ src/js/listeners/deleteAuction.mjs
@@ -1,19 +1,14 @@
 import { baseUrl } from "../api/apiUrls.mjs";
-import { deleteAPI } from "../api/apiCalls/auctions/delete.mjs";
 import { getParams } from "../func/params.mjs";
+import { deleteAPI } from "../api/apiCalls/auctions/delete.mjs";
 
 export async function deleteAuction() {
   const deleteBtn = document.getElementById("deleteBtn");
-  const errorMessage = document.querySelector("deleteMessage");
 
   deleteBtn.addEventListener("click", async (event) => {
     event.preventDefault();
 
     const id = getParams("id");
-    const response = await deleteAPI(`${baseUrl}/auction/listings/${id}`);
-
-    if (response.errors) {
-      errorMessage.innerText = "Something went wrong! Try again later.";
-    }
+    await deleteAPI(`${baseUrl}/auction/listings/${id}`);
   });
 }
