0e2fa8d54d48316fe3ff0b43c18efecd21f1dbf8
mathildeew <mathildewiik@hotmail.com>
2023-03-05 15:59:26 +0100

    moved errormess from register listener to reg api

--- src/js/api/apiCalls/auth/register.mjs
+++ src/js/api/apiCalls/auth/register.mjs
@@ -5,25 +5,30 @@ import { fetchOptions } from "../../fetchOptions.mjs";
  * Sends a POST request to the server
  * @param {url} url Register url
  * @param {object} postContent The body of the request
- * @returns The response json from the request
  * @example
  * ```
  * // Sends a POST request with register credentials.
- * // Returns the json as response
+ * // Redirects to log in if successfull
+ * // Shows error messages if not successfull
  * const response = await register(`${baseUrl}/auction/auth/register`, postContent)
  *```
  */
 export async function register(url, postContent) {
+  const registerBtn = document.getElementById("registerBtn");
+  const errorMessage = document.querySelector(".errorMessage");
+
   const [getData, postData] = fetchOptions;
   postData["body"] = JSON.stringify(postContent);
   const response = await fetch(url, postData);
   const json = await response.json();
 
-  console.log(json);
-
-  if (response.ok === true) {
-    setStorage(json);
+  if (response.ok) {
     window.location.href = "/accounts/login/";
   }
-  return json;
+
+  if (json.errors) {
+    errorMessage.style.display = "block";
+    errorMessage.innerText = `${json.errors[0].message}`;
+    registerBtn.innerHTML = "Register";
+  }
 }
