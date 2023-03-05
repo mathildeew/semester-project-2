717eae51e03124d67edef70eaac44c912fac4dee
mathildeew <mathildewiik@hotmail.com>
2023-03-05 15:21:59 +0100

    adds jsdocs

--- src/js/api/apiCalls/auctions/loadMore.mjs
+++ src/js/api/apiCalls/auctions/loadMore.mjs
@@ -4,6 +4,11 @@ import { baseUrl } from "../../apiUrls.mjs";
 import * as loader from "../../../components/loader.mjs";
 import * as loadMoreBtn from "../../../func/loadMoreButton.mjs";
 
+/**
+ * Sends a request to the server to load more content
+ * Limit controls how many objects that's returned
+ * Offset controls how many objects thats's skipped in the returned array
+ */
 let limit = 6;
 let offset = 6;
 
@@ -12,9 +17,12 @@ export async function loadMore() {
   loadMoreBtn.hideLoadMore();
 
   const link = `${baseUrl}/auction/listings?sort=created&sortOrder=desc&_seller=true&_bids=true&_active=true&limit=${limit}&offset=${offset}`;
+
   const response = await fetch(link);
   const json = await response.json();
+
   offset = offset + limit;
+
   if (response.ok) {
     display(json);
     loader.hideLoader();
