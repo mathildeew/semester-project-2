3579560cd2843c0fb633ede6955ad942f7ed8837
mathildeew <mathildewiik@hotmail.com>
2023-03-05 15:30:34 +0100

    updates jsdocs

--- src/js/api/apiCalls/auth/login.mjs
+++ src/js/api/apiCalls/auth/login.mjs
@@ -5,11 +5,11 @@ import { fetchOptions } from "../../fetchOptions.mjs";
  * Sends a POST request to the server
  * @param {url} url Log in url
  * @param {object} postContent The body of the request
- * @returns The response from the request
+ * @returns The returned response json
  * @example
  * ```
  * // Sends a POST request with log in credentials.
- * // Returns the json as response
+ * // Stores the json in localStorage if successfull.
  * const response = await login(`${baseUrl}/auction/auth/login`, postContent)
  *```
  */
@@ -19,7 +19,7 @@ export async function login(url, postContent) {
   const response = await fetch(url, postData);
   const json = await response.json();
 
-  if (response.ok === true) {
+  if (response.ok) {
     setStorage(json);
     window.location.href = "/";
   }
